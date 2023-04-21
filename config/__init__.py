0197a323fd1d6920e9005453ded7b9d1ee53c284
VoidMine <116156699+Infosec-Void@users.noreply.github.com>
2023-04-21 11:41:03 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by Infosec-Void@Github, < https://github.com/Infosec-Void >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/Infosec-Void/Voidmusicbot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/Infosec-Void/Voidmusicbot/blob/master/LICENSE >
 #
 # All rights reserved.
 #
