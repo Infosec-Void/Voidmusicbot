8f963983bf3c22b4521431ee7e81982451c50069
VoidMine <116156699+Infosec-Void@users.noreply.github.com>
2023-04-21 11:42:07 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/Infosec-Void >.
+# Copyright (C) 2021-2022 by Infosec-Void@Github, < https://github.com/Infosec-Void >.
 #
 # This file is part of < https://github.com/Infosec-Void/Voidmusicbot > project,
 # and is released under the "GNU v3.0 License Agreement".
